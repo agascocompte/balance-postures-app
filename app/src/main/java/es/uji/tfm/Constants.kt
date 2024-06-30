fc83c4114cf1ed3330f7aac348a949e367e35960
agascocompte <agasco@uji.es>
2024-07-01 01:27:50 +0200

    feat: testing custom model

--- app/src/main/java/es/uji/tfm/Constants.kt
+++ app/src/main/java/es/uji/tfm/Constants.kt
@@ -1,6 +1,7 @@
 package es.uji.tfm
 
 object Constants {
-    const val MODEL_PATH = "yolov8s-seg_int8.tflite"
+    //const val MODEL_PATH = "yolov8s-seg_int8.tflite"
+    const val MODEL_PATH = "custom-yolov8n-seg_int8.tflite"
     const val LABELS_PATH = "labels.txt"
 }
