4d50e1e7b67c40af791f53861a657f0d627d513f
agascocompte <agasco@uji.es>
2024-07-01 15:27:20 +0200

    feat: set basic segmentation with yolv8s-seg model

--- app/src/main/java/es/uji/tfm/Constants.kt
+++ app/src/main/java/es/uji/tfm/Constants.kt
@@ -1,7 +1,7 @@
 package es.uji.tfm
 
 object Constants {
-    //const val MODEL_PATH = "yolov8s-seg_int8.tflite"
-    const val MODEL_PATH = "custom-v3-yolov8n-seg_int8.tflite"
+    const val MODEL_PATH = "yolov8s-seg_int8.tflite"
+    //const val MODEL_PATH = "custom-v3-yolov8n-seg_int8.tflite"
     const val LABELS_PATH = "labels.txt"
 }
