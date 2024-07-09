103724d3f5f9ba19c6cc9c17b28ca2be883d01f4
agascocompte <agasco@uji.es>
2024-07-09 18:40:22 +0200

    feat: added more models for testing

--- app/src/main/java/es/uji/tfm/Constants.kt
+++ app/src/main/java/es/uji/tfm/Constants.kt
@@ -2,6 +2,10 @@ package es.uji.tfm
 
 object Constants {
     //const val MODEL_PATH = "yolov8s-seg_int8.tflite"
-    const val MODEL_PATH = "custom-v3-yolov8n-seg_int8.tflite"
-    const val LABELS_PATH = "balance_labels.txt"
+    //const val MODEL_PATH = "custom-v3-yolov8n-seg_int8.tflite" // Right one
+    const val MODEL_PATH = "custom_yolo_6p_v8n_int8.tflite"
+    //const val MODEL_PATH = "custom_yolo_6p_v8s_int8.tflite"
+
+    //const val LABELS_PATH = "balance_labels.txt"
+    const val LABELS_PATH = "balance_labels_6p.txt"
 }
