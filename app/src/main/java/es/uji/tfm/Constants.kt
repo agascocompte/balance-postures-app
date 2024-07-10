a2463f65d3e921c95998b09e19a144f1b5337088
agascocompte <agasco@uji.es>
2024-07-10 17:49:46 +0200

    feat: changed switch camera icons, solved inferenceTime appearance and added 224x224 models

--- app/src/main/java/es/uji/tfm/Constants.kt
+++ app/src/main/java/es/uji/tfm/Constants.kt
@@ -1,11 +1,7 @@
 package es.uji.tfm
 
 object Constants {
-    //const val MODEL_PATH = "yolov8s-seg_int8.tflite"
-    //const val MODEL_PATH = "custom-v3-yolov8n-seg_int8.tflite" // Right one
-    const val MODEL_PATH = "custom_yolo_6p_v8n_int8.tflite"
-    //const val MODEL_PATH = "custom_yolo_6p_v8s_int8.tflite"
-
-    //const val LABELS_PATH = "balance_labels.txt"
+    const val MODEL_PATH = "custom_yolo_6p_v8n_224_int8.tflite" // 25-60 ms
+    //const val MODEL_PATH = "custom_yolo_6p_v8s_224_int8.tflite" // 80 - 102 ms
     const val LABELS_PATH = "balance_labels_6p.txt"
 }
