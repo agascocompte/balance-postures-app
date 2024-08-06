30ad857c468792b35f15c0457ca13ff9426b7709
agascocompte <agasco@uji.es>
2024-08-06 10:36:32 +0200

    feat: added no detectition feature

--- app/src/main/java/es/uji/tfm/Constants.kt
+++ app/src/main/java/es/uji/tfm/Constants.kt
@@ -1,7 +1,6 @@
 package es.uji.tfm
 
 object Constants {
-    const val MODEL_PATH = "custom_yolo_6p_v8n_224_int8.tflite" // 25-60 ms
-    //const val MODEL_PATH = "custom_yolo_6p_v8s_224_int8.tflite" // 80 - 102 ms
+    const val MODEL_PATH = "custom_yolo_7p_40_v8n_224_int8.tflite" // 40 images
     const val LABELS_PATH = "balance_labels_6p.txt"
 }
